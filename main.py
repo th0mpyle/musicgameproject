a7f0007342d57d1d8bd21706cd5a6da9a56ffb78
thom pyle <99365003+th0mpyle@users.noreply.github.com>
2022-04-25 11:28:05 +0100

    error test in main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # main
+
+songs = open("songs.txt", "r")
+songs = songs.readlines()
+
+passwords = open("passwords.txt", "r")
+passwords = passwords.readlines()
+
+if __name__ == "__main__":
+    print("no error found")
+    print(songs)
